c4b08fe313bcfe2475d4701c5ac38f2b2aa7d34e
mayor167 <adenijimayokun@gmail.com>
2024-01-02 23:20:21 +0100

    done

--- 0x00-shell_basics/README.md
+++
@@ -1,1 +0,0 @@
-engineeering devops
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,8 +0,0 @@
-# Script to Kill a process name killmenow
-
-exec { 'pkill killmenow':
-  path     => '/usr/bin',
-  command  => 'pkill killmenow',
-  provider => shell,
-  returns  => [0, 1]
-}
